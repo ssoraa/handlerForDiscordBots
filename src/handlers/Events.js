15ea9752ca5cea67337c17f96b4fcbfe0e281289
KiuyOR <kiuy.or@icloud.com>
2020-06-05 12:18:48 +0200

    up events

--- src/handlers/Events.js
+++ src/handlers/Events.js
@@ -1,36 +1,37 @@
-const {readdir} = require('fs');
+const {readdir} = require('fs'),
+    {resolve} = require('path'),
+    chalk = require('chalk')
 
+/**
+ * @author Sora
+ * @param client
+ * @return {Promise<void>}
+ */
 module.exports = async (client) => {
-    readdir("./commands/", (err, content) => {
+    readdir("./events/", (err, content) => {
         if (err) console.log(err);
+        if (content.length < 1) return;
         let groups = [];
-        content.forEach(element => {
+        for (let element of content){
             if (!element.includes('.')) groups.push(element);
-        });
-        groups.forEach(folder => {
-            readdir("./commands/" + folder, (e, files) => {
-                let js_files = files.filter(f => f.split(".").pop() === "js");
-                if (js_files.length < 1) return;
+        }
+        for (const folder of groups) {
+            readdir("./events/" + folder, (e, files) => {
+                let eventFile = files.filter(f => f.split(".").pop() === "js");
+                if (eventFile.length < 1) return;
                 if (e) console.log(e);
-                js_files.forEach(element => {
-                    let props = require('./commands/' + folder + '/' + element);
-                    console.log(`Loading Command: ${element.split('.')[0]}`)
-
+                for (const element of eventFile) {
+                    let props = require(resolve('./events/' + folder + '/' + element));
+                    console.log(`${chalk.green('[LOAD]')} ${chalk.blue(element.split('.')[0])} ${chalk.yellow('EVENT')}`)
                     if (process.stdout.moveCursor) {
                         process.stdout.moveCursor(0, -1);
                     }
                     if (process.stdout.clearLine) {
                         process.stdout.clearLine();
                     }
-
-                    client.commands.set(element.split('.')[0], props);
-                    if (props.conf && props.conf.aliases) {
-                        props.conf.aliases.forEach(alias => {
-                            client.aliases.set(alias, props)
-                        });
-                    }
-                });
+                    client.on(element.split('.')[0], props.bind(null, client));
+                }
             });
-        });
+        }
     });
 };
